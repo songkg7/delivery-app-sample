7c2fab587b1e88b305ae18c2b6fd5e1527f352db
haril song <songkg7@gmail.com>
2023-04-05 10:15:54 +0900

    Initial commit

---
+++ src/test/kotlin/com/haril/deliveryappsample/DeliveryAppSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.haril.deliveryappsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeliveryAppSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
